7e5e1ec113ed8996bbf652a5790a153eb1f5d515
Pavel Pshen <memasyt228@gmail.com>
2022-05-26 20:57:51 +0300

    5

--- app/src/androidTest/java/com/example/pocketHelper/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/pocketHelper/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.pocketHelper;
 
 import android.content.Context;
 
--- app/src/test/java/com/example/pocketHelper/ExampleUnitTest.java
+++ app/src/test/java/com/example/pocketHelper/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.pocketHelper;
 
 import org.junit.Test;
 
